41a1bfb2911460f1e9e2a9602518e8f309af99d7
Ismail Halili <ihalili.mmt-b2019@fh-salzburg.ac.at>
2021-03-09 21:25:00 +0100

    add inverted index as a Map to the Searcher

--- searcher.js
+++ searcher.js
@@ -1,10 +1,50 @@
+import fs from "fs";
+import { doc } from "prettier";
+
 export default class Searcher {
     constructor(files) {
         // TODO: Build the Inverted Index.
+        this.invertedIndex = this.getInvertedIndexFromFiles(files);
     }
 
     search(query) {
         // TODO: Search the indexed files.
         return [];
+    }
+
+    getInvertedIndexFromFiles(files) {
+        try {
+            const invertedIndex = new Map();
+
+            files.forEach(file => {
+                const documentText = fs.readFileSync(file, "utf-8");
+                const terms = this.termifyText(documentText);
+
+                terms.forEach(term => {
+                    if (invertedIndex.has(term)) {
+                        const postingList = new Set(invertedIndex.get(term));
+                        postingList.add(file);
+                        invertedIndex.set(term, [...postingList]);
+                    } else {
+                        invertedIndex.set(term, [file]);
+                    }
+                });
+            });
+
+            return invertedIndex;
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    termifyText(text) {
+        const punctuationRegex = /[!"#$%&'()*+,./:;<=>?@[\]^_`{|}~]/g;
+        const newLineRegex = /(\r\n|\n|\r)/gm;
+
+        return text
+            .replace(punctuationRegex, "")
+            .replace(newLineRegex, "")
+            .toLowerCase()
+            .split(" ");
     }
 }
